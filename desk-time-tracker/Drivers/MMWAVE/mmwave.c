37b1cf36cea6bbd820e7d94b2cc1c2d25e96998e
Noah Robertson <nhrobertson4@gmail.com>
2026-02-26 22:42:22 -0500

    Work on mmwave, need to setup configuration so its not sensing me accross the room xD

--- desk-time-tracker/Drivers/MMWAVE/mmwave.c
+++ desk-time-tracker/Drivers/MMWAVE/mmwave.c
@@ -8,8 +8,22 @@
 #include "mmwave.h"
 
 mmwave_Data_t mmwave_Data;
+uint8_t tx_buff[32];
 
-void mmwave_Init() {
+
+void mmwave_Write_Command(mmwave_Packet packet, UART_HandleTypeDef huart1) {
+
+	tx_buff[0] = packet.header;
+	tx_buff[1] = packet.data_len;
+	tx_buff[2] = packet.cmd_word;
+	for (int i = 0; i < data_len; ++i) {
+		tx_buff[3 + i] = packet.cmd_value[i];
+	}
+	tx_buff[3 + packet.data_len + 1] = packet.footer;
+	HAL_UART_Transmit(&huart, tx_buff, packet.data_len, 1000);
+}
+
+void mmwave_Init(UART_HandleTypeDef huart1) {
 
 }
 
--- desk-time-tracker/Drivers/MMWAVE/mmwave.h
+++ desk-time-tracker/Drivers/MMWAVE/mmwave.h
@@ -17,12 +17,41 @@
 
 #define BUFFER_SIZE 23 //The size of a data frame in default mode
 
-#define HEADER_SEQUENCE 0xf4f3f2f1
+
+//Configuration
+
+//Send command protocol
+//Frame Header | 2 byte intra-frame data length | Intra-frame data | End of frame
+
+#define MMWAVE_CONFIG_HEADER 0xFDFCFBFA
+#define MMWAVE_CONFIG_FOOTER 0x04030201
+
+#define MMWAVE_ENABLE_CONFIG_CW 0x00FF
+#define MMWAVE_ENABLE_CONFIG_CV 0x0001
+
+#define MMWAVE_DISABLE_CONFIG_CW 0x00FE
+
+#define MMWAVE_MAX_DIST_GATE_CW 0x0060
+//Command Value -> 2 byte max motion distance gate word + 4 byte max motion distance gate param + 2 byte max standstill distance gate word + 4 byte max standstill distance gate param + 2 byte unoccupied duration word + 4 byte unoccupied duration param
+
+#define MMWAVE_DISTANCE_RESOLUTION_CW 0x00AA
+//Command Value -> 2 byte distance resolution selection index (0x0000 -> 0.75m, 0x0001 -> 0.2m (but fd is 0x0001 which is 0.75m))
+
+
+#define HEADER_SEQUENCE 0xF4F3F2F1
 //Header
 //0xF4F3F2F1
 
 //Final Sequence
 //0xF8F7F6F5
+
+typedef struct {
+	uint8_t header;
+	uint8_t data_len;
+	uint8_t cmd_word;
+	uint8_t cmd_value[20];
+	uint8_t footer;
+} mmwave_Packet;
 
 typedef struct {
     uint8_t state; //0 - None, 1 - Campaign, 2 - Stationary, 3 - Both, any other is Invalid
@@ -31,8 +60,8 @@ typedef struct {
 
 extern mmwave_Data_t mmwave_Data;
 
-
-void mmwave_Init(void);
+void mmwave_Write_Command(mmwave_Packet packet, UART_HandleTypeDef huart1);
+void mmwave_Init(UART_HandleTypeDef huart1);
 int mmwave_Recieve(UART_HandleTypeDef huart1);
 int mmwave_Parse();
 
